X11Libre/xf86-video-ati

commit ce7db51020d32f17e442338bfd305220feb51630

Cancel pending scanout update in drmmode_crtc_scanout_update
drmmode_crtc_scanout_update does the equivalent of a scanout update,
so no need to do it again. This might also avoid issues if there's a
pending scanout update at this point.

(Ported from amdgpu commit 4e7a24ac5a64e402146953ec5850d13c05742116)

src/drmmode_display.c
@@ -781,11 +781,17 @@ drmmode_crtc_scanout_update(xf86CrtcPtr crtc, DisplayModePtr mode,
 		*fb = radeon_pixmap_get_fb(drmmode_crtc->scanout[scanout_id].pixmap);
 		*x = *y = 0;
 
-		radeon_scanout_do_update(crtc, scanout_id,
-					 screen->GetWindowPixmap(screen->root),
-					 extents);
-		RegionEmpty(DamageRegion(drmmode_crtc->scanout_damage));
-		radeon_finish(scrn, drmmode_crtc->scanout[scanout_id].bo);
+		if (radeon_scanout_do_update(crtc, scanout_id,
+					     screen->GetWindowPixmap(screen->root),
+					     extents)) {
+			RegionEmpty(DamageRegion(drmmode_crtc->scanout_damage));
+			radeon_glamor_finish(scrn);
+
+			if (!drmmode_crtc->flip_pending) {
+				radeon_drm_abort_entry(drmmode_crtc->
+						       scanout_update_pending);
+			}
+		}
 	}
 }
 

commit 79bc0e054f37026377d54cac6cd8127d4aa9baca

Use radeon_finish in drmmode_crtc_scanout_update
radeon_glamor_finish only works if we're using glamor, otherwise it'll
crash.

Fixes: ce7db51020d3 "Cancel pending scanout update in drmmode_crtc_scanout_update"
Bug: https://bugs.debian.org/924540
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

src/drmmode_display.c
@@ -785,7 +785,7 @@ drmmode_crtc_scanout_update(xf86CrtcPtr crtc, DisplayModePtr mode,
 					     screen->GetWindowPixmap(screen->root),
 					     extents)) {
 			RegionEmpty(DamageRegion(drmmode_crtc->scanout_damage));
-			radeon_glamor_finish(scrn);
+			radeon_finish(scrn, drmmode_crtc->scanout[scanout_id].bo);
 
 			if (!drmmode_crtc->flip_pending) {
 				radeon_drm_abort_entry(drmmode_crtc->