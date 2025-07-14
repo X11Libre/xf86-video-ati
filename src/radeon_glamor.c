445749d1981798820ba71b17549ff9d2a57e8645
Enrico Weigelt, metux IT consult <info@metux.net>
2025-07-14 22:13:37 +0200

    drop support for xserver version < 1.20.99
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/radeon_glamor.c
+++ src/radeon_glamor.c
@@ -104,11 +104,7 @@ radeon_glamor_pre_init(ScrnInfoPtr scrn)
 			   "glamor may not work (well) with GPUs < RV515.\n");
 	}
 
-#if XORG_VERSION_CURRENT < XORG_VERSION_NUMERIC(1,20,99,0,0)
-	if (scrn->depth < 24) {
-#else
 	if (scrn->depth < 15) {
-#endif
 		xf86DrvMsg(scrn->scrnIndex, s ? X_ERROR : X_WARNING,
 			   "Depth %d not supported with glamor, disabling\n",
 			   scrn->depth);
